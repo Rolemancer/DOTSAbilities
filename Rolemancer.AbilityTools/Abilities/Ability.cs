0abcb1b19e25d0acde150e9caa2ccc58ee17a75f
Volkov Yaroslav <newsomebox@gmail.com>
2022-01-16 15:29:21 +0300

    Ability first steps

--- Rolemancer.AbilityTools/Abilities/Ability.cs
+++ Rolemancer.AbilityTools/Abilities/Ability.cs
@@ -1,12 +1,47 @@
-﻿using Unity.Burst;
+﻿using Rolemancer.AbilityTools.Base;
+using Rolemancer.AbilityTools.Effects;
+using Unity.Burst;
 using Unity.Collections;
 
 namespace Rolemancer.AbilityTools.Abilities
 {
     [BurstCompatible]
     [BurstCompile]
-    public struct Ability
+    public struct Ability : ITypeWithDbKey<AbilityDBKey>
     {
-        public AbilityDBKey DbKey;
+        public bool Enabled;
+        public float Cooldown;
+        public float CooldownFinishTime;
+        public EffectDBKey EffectToApply;
+        
+        public AbilityDBKey DbKey { get; }
+
+        public Ability(AbilityDBKey key, EffectDBKey effect)
+        {
+            DbKey = key;
+            Enabled = true;
+            Cooldown = default;
+            CooldownFinishTime = default;
+            EffectToApply = effect;
+        }
+
+        public void EnableCooldown(float currentTime)
+        {
+            CooldownFinishTime = currentTime + Cooldown;
+            if (currentTime < CooldownFinishTime)
+                Enabled = false;
+        }
+        
+        public void EnableCustomCooldown(float currentTime, float finishTime)
+        {
+            CooldownFinishTime = finishTime;
+            
+        }
+    }
+
+    [BurstCompatible]
+    [BurstCompile]
+    public struct AbilityInstance
+    {
     }
 }
---
+++ Rolemancer.AbilityTools/Abilities/AbilityProcessing.cs
@@ -0,0 +1,80 @@
+﻿using Rolemancer.AbilityTools.DataMapping;
+using Rolemancer.AbilityTools.Effects;
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace Rolemancer.AbilityTools.Abilities
+{
+    public static class AbilityProcessing
+    {
+        public static void Process(double elapsedTime)
+        {
+            var map = Mapping.Map;
+
+            // Pending
+            var pending = new PendingJob { Map = map, ElapsedTime = elapsedTime };
+            var pendingHandle = pending.Schedule();
+            // Update Life Time
+            var checkLifeTime = new CheckLifeTime { Map = map, ElapsedTime = elapsedTime };
+            var checkLifeTimeHandle = checkLifeTime.Schedule(pendingHandle);
+
+            pendingHandle.Complete();
+            checkLifeTimeHandle.Complete();
+        }
+
+        private struct PendingJob : IJob
+        {
+            public DataMap Map;
+            public double ElapsedTime;
+
+            public void Execute()
+            {
+                var effectsCollection = Map.Effects.GetAllEffects();
+                var keys = effectsCollection.GetKeyArray(Allocator.Temp);
+
+                for (var i = 0; i < keys.Length; i++)
+                {
+                    var key = keys[i];
+                    var effect = effectsCollection[key];
+                    if (effect.Status != EffectStatus.Creating)
+                        continue;
+                    effect.Status = EffectStatus.Pending;
+                    if (effect.ApplyMode == EffectApplyMode.LongLife)
+                        effect.DiscardTime = ElapsedTime + effect.LifeTime;
+                    effectsCollection[key] = effect;
+                }
+
+                keys.Dispose();
+            }
+        }
+
+        private struct CheckLifeTime : IJob
+        {
+            public DataMap Map;
+            public double ElapsedTime;
+
+            public void Execute()
+            {
+                var effectsCollection = Map.Effects.GetAllEffects();
+                var keys = effectsCollection.GetKeyArray(Allocator.Temp);
+                for (var i = 0; i < keys.Length; i++)
+                {
+                    var key = keys[i];
+                    var effect = effectsCollection[key];
+
+                    if (effect.ApplyMode == EffectApplyMode.Immediate)
+                        continue;
+
+                    if (effect.Status == EffectStatus.Discarded)
+                        continue;
+
+                    var finishTime = effect.DiscardTime;
+
+                    if (ElapsedTime >= finishTime) key.DiscardEffect(Map);
+                }
+
+                keys.Dispose();
+            }
+        }
+    }
+}
